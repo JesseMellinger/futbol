66e620aef120b6ef5dabef93346d99a1bf9dbc23
Eduardo Parra <helloeduardo@icloud.com>
2020-09-05 11:09:29 -0600

    Add test for team information

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -49,4 +49,17 @@ class StatTrackerTest < Minitest::Test
     assert_equal expected, @stat_tracker.game_teams
   end
 
+  # ************** Team Statistics Tests **************
+
+  def test_it_has_team_info
+    expected = {
+      "team_id" => "4",
+      "franchise_id" => "16",
+      "team_name" => "Chicago Fire",
+      "abbreviation" => "CHI",
+      "link" => "/api/v1/teams/4"
+    }
+    assert_equal expected, @stat_tracker.team_info("4")
+  end
+
 end
