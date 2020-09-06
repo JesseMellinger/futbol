c4f02d1d1a57ed0b25d294f077ee0153bc56a461
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-06 11:33:10 -0600

    Update indentation in worst_offense method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -49,7 +49,7 @@ class StatTracker
     team_id = group_by(@game_teams, "team_id", "goals").min_by do |team_id, goals_in_game|
       goals_in_game.map(&:to_i).sum.to_f / (goals_in_game.length)
     end.first
-  @teams.find {|row| row["team_id"] == team_id}["teamName"]
+    @teams.find {|row| row["team_id"] == team_id}["teamName"]
   end
 
 end
