c63968bd228895b25e2d181420af09b69f212d3d
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-08 12:17:22 -0600

    Refactoring original code to use season argument

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -46,197 +46,223 @@ class StatTracker
     @teams.find {|row| row["team_id"] == team_id}["teamName"]
   end
 # ************* Season Statistics *************
-  def total_games_played_by_coach_helper
-    games_coached_hash =  @game_teams["head_coach"].group_by(&:itself)
-        games_coached_hash.map{|key, value| [key, value.length]}.to_h
-      end
 
-  def total_games_won_by_coach_array_helper
-    games_won_array = []
-    @game_teams.each do |row|
-      if row[3] == "WIN"
-        games_won_array << row[5]
+def game_id_by_season_helper(season)
+  season_by_game_id_hash= {}
+  @games.each do |row|
+    if row[1] == season
+        season_by_game_id_hash[row[0]] = row[1]
       end
     end
-    games_won_array
+   season_by_game_id_hash
+ end
+
+  def total_games_played_by_coach_helper(season)
+  coaches_total_games_array = []
+  season_by_game_id_hash = game_id_by_season_helper(season).keys
+  @game_teams.each do |row|
+      season_by_game_id_hash.each do |game_id|
+        if row[0] == (game_id)
+    coaches_total_games_array << row[5]
+        end
+      end
+    end
+    coaches_total_games_array.group_by(&:itself)
+      games_played_hash.map{|key, value| [key, value.length]}.to_h
   end
 
-  def games_won_into_hash_helper
-  games_won_hash = total_games_won_by_coach_array_helper.group_by(&:itself)
+  def total_games_played_into_hash_helper(season)
+
+    games_played_hash = total_games_played_by_coach_helper(season).group_by(&:itself)
+      games_played_hash.map{|key, value| [key, value.length]}.to_h
+  end
+
+  def total_games_won_by_coach_array_helper(season)
+    coaches_total_wins_array = []
+    season_by_game_id_hash = game_id_by_season_helper(season).keys
+    @game_teams.each do |row|
+        season_by_game_id_hash.each do |game_id|
+          if row[0] == (game_id) && row[3] == "WIN"
+      coaches_total_wins_array << row[5]
+          end
+        end
+      end
+      coaches_total_wins_array
+  end
+
+  def games_won_into_hash_helper(season)
+  games_won_hash = total_games_won_by_coach_array_helper(season).group_by(&:itself)
     games_won_hash.map{|key, value| [key, value.length]}.to_h
   end
 
-  def coaches_with_games_played_and_won_array_helper
-      coaches_with_games_played_array = total_games_played_by_coach_helper.keys
-      coaches_with_games_won_array = games_won_into_hash_helper.keys
+  def coaches_with_games_played_and_won_array_helper(season)
+      coaches_with_games_played_array = total_games_played_by_coach_helper(season)
+      coaches_with_games_won_array = games_won_into_hash_helper(season).keys
       coaches_with_games_won_and_played_array = coaches_with_games_played_array & coaches_with_games_won_array
       coaches_with_games_won_and_played_array
   end
 
-  def coaches_winning_percentage_hash_helper
+  def coaches_winning_percentage_hash_helper(season)
     coaches_winning_percentage_hash = {}
-    coaches_with_games_played_and_won_array_helper.each do |coach|
+    coaches_with_games_played_and_won_array_helper(season).each do |coach|
 
-      coaches_winning_percentage_hash[coach] = ((games_won_into_hash_helper[coach] / total_games_played_by_coach_helper[coach] * 100))
+    coaches_winning_percentage_hash[coach] = ((games_won_into_hash_helper(season)[coach].to_f / total_games_played_into_hash_helper(season)[coach].to_f * 100))
     end
     coaches_winning_percentage_hash
   end
 
-  def winningest_coach
-    name_of_coach_with_number_variable = coaches_winning_percentage_hash_helper.find_all do |key, value|
-      value == coaches_winning_percentage_hash_helper.values.max
+  def winningest_coach(season)
+    name_of_coach_with_number_variable = coaches_winning_percentage_hash_helper(season).find_all do |key, value|
+      value == coaches_winning_percentage_hash_helper(season).values.max
+      coaches_winning_percentage_hash_helper(season).key(name_of_coach_with_number_variable)
     end
-    name_of_coach_array_variable = []
-  name_of_coach_with_number_variable.each do |name_and_number|
-  name_of_coach_array_variable << name_and_number[0]
-    end
-    name_of_coach_array_variable.join(", ")
-  end
-
-  def worst_coach_if_someone_doesnt_have_any_wins_helper
-    worst_coach_array = []
-    total_games_played_by_coach_helper.keys.each do |coach|
-      if !games_won_into_hash_helper.keys.include?(coach)
-        worst_coach_array << coach
-      end
-    end
-    worst_coach_array
-  end
-
-  def worst_coach_if_everyone_has_a_win_helper
-
-    name_of_coach_with_number_variable =
-      coaches_winning_percentage_hash_helper.find_all do |key, value| value == coaches_winning_percentage_hash_helper.values.min
-    end
-    name_of_coach_array_variables = []
-    name_of_coach_with_number_variable.each do |name_and_number|
-    name_of_coach_array_variables << name_and_number[0]
-    end
-  name_of_coach_array_variables
- end
-
- def worst_coach
-
-  if worst_coach_if_someone_doesnt_have_any_wins_helper.length == 0
-     worst_coach_if_everyone_has_a_win_helper.join(", ")
-  else
-     worst_coach_if_someone_doesnt_have_any_wins_helper.join(", ")
   end
 end
 
-  def total_goals_by_team_id_hash_helper
-    total_goals_by_team_hash = {}
-    @game_teams.each do |row|
-      if total_goals_by_team_hash.include?(row[1])
-        total_goals_by_team_hash[row[1]] += row[6].to_i
-      else
-        total_goals_by_team_hash[row[1]] = row[6].to_i
-      end
-    end
- total_goals_by_team_hash
-  end
-
-  def total_shots_by_team_id_hash_helper
-    total_shots_by_team_hash = {}
-    @game_teams.each do |row|
-      if total_shots_by_team_hash.include?(row[1])
-        total_shots_by_team_hash[row[1]] += row[7].to_i
-      else
-        total_shots_by_team_hash[row[1]] = row[7].to_i
-      end
-    end
- total_shots_by_team_hash
-  end
-
-  def ratio_of_shots_to_goals_by_team_id_helper
-    total_goals_by_team_id_hash_helper.merge!(total_shots_by_team_id_hash_helper) {|key, value1, value2| (value1.to_f / value2.to_f).round(2)}
-  end
-
-  def highest_win_percentage_by_team_id_helper
-    team_id_with_ratio_of_shot = ratio_of_shots_to_goals_by_team_id_helper.find_all do |key, value|
-      value == ratio_of_shots_to_goals_by_team_id_helper.values.max
-    end
-    team_id_variable = []
-  team_id_with_ratio_of_shot.each do |id_and_ratio|
-  team_id_variable << id_and_ratio[0]
-    end
-    team_id_variable
-  end
-
-  def most_accurate_team
-    team_name = @teams.find do |row|
-      row[0] == highest_win_percentage_by_team_id_helper[0]
-    end
-    team_name[2]
-  end
-
-  def lowest_win_percentage_by_team_id_helper
-
-    team_id_with_ratio_of_shot = ratio_of_shots_to_goals_by_team_id_helper.find_all do |key, value|
-      value == ratio_of_shots_to_goals_by_team_id_helper.values.min
-    end
-    team_id_variable = []
-  team_id_with_ratio_of_shot.each do |id_and_ratio|
-  team_id_variable << id_and_ratio[0]
-    end
-    team_id_variable
-  end
-
-  def least_accurate_team
-    team_name = @teams.find do |row|
-      row[0] == lowest_win_percentage_by_team_id_helper[0]
-    end
-    team_name[2]
-  end
-
-  def total_number_tackles_by_team_id_helper
-
-      total_tackles_by_team_hash = {}
-      @game_teams.each do |row|
-        if total_tackles_by_team_hash.include?(row[1])
-          total_tackles_by_team_hash[row[1]] += row[8].to_i
-        else
-          total_tackles_by_team_hash[row[1]] = row[8].to_i
-        end
-      end
-   total_tackles_by_team_hash
-  end
-
-  def team_id_with_most_tackles_helper
-
-    team_id_with_total_tackles = total_number_tackles_by_team_id_helper.find_all do |key, value|
-      value == total_number_tackles_by_team_id_helper.values.max
-    end
-    team_id_variable = []
-  team_id_with_total_tackles.each do |id_and_ratio|
-  team_id_variable << id_and_ratio[0]
-    end
-    team_id_variable
-  end
-
-  def most_tackles
-    team_name = @teams.find do |row|
-      row[0] == team_id_with_most_tackles_helper[0]
-    end
-    team_name[2]
-  end
-
-  def team_id_with_fewest_tackles_helper
-
-    team_id_with_total_tackles = total_number_tackles_by_team_id_helper.find_all do |key, value|
-      value == total_number_tackles_by_team_id_helper.values.min
-    end
-    team_id_variable = []
-  team_id_with_total_tackles.each do |id_and_ratio|
-  team_id_variable << id_and_ratio[0]
-    end
-    team_id_variable
-  end
-
-  def fewest_tackles
-    team_name = @teams.find do |row|
-      row[0] == team_id_with_fewest_tackles_helper[0]
-    end
-    team_name[2]
-    end
-  end
+#   def worst_coach_if_someone_doesnt_have_any_wins_helper
+#     worst_coach_array = []
+#     total_games_played_by_coach_helper.keys.each do |coach|
+#       if !games_won_into_hash_helper.keys.include?(coach)
+#         worst_coach_array << coach
+#       end
+#     end
+#     worst_coach_array
+#   end
+#
+#   def worst_coach_if_everyone_has_a_win_helper
+#
+#     name_of_coach_with_number_variable =
+#       coaches_winning_percentage_hash_helper.find_all do |key, value| value == coaches_winning_percentage_hash_helper.values.min
+#     end
+#     name_of_coach_array_variables = []
+#     name_of_coach_with_number_variable.each do |name_and_number|
+#     name_of_coach_array_variables << name_and_number[0]
+#     end
+#   name_of_coach_array_variables
+#  end
+#
+#  def worst_coach
+#
+#   if worst_coach_if_someone_doesnt_have_any_wins_helper.length == 0
+#      worst_coach_if_everyone_has_a_win_helper.join(", ")
+#   else
+#      worst_coach_if_someone_doesnt_have_any_wins_helper.join(", ")
+#   end
+# end
+#
+#   def total_goals_by_team_id_hash_helper
+#     total_goals_by_team_hash = {}
+#     @game_teams.each do |row|
+#       if total_goals_by_team_hash.include?(row[1])
+#         total_goals_by_team_hash[row[1]] += row[6].to_i
+#       else
+#         total_goals_by_team_hash[row[1]] = row[6].to_i
+#       end
+#     end
+#  total_goals_by_team_hash
+#   end
+#
+#   def total_shots_by_team_id_hash_helper
+#     total_shots_by_team_hash = {}
+#     @game_teams.each do |row|
+#       if total_shots_by_team_hash.include?(row[1])
+#         total_shots_by_team_hash[row[1]] += row[7].to_i
+#       else
+#         total_shots_by_team_hash[row[1]] = row[7].to_i
+#       end
+#     end
+#  total_shots_by_team_hash
+#   end
+#
+#   def ratio_of_shots_to_goals_by_team_id_helper
+#     total_goals_by_team_id_hash_helper.merge!(total_shots_by_team_id_hash_helper) {|key, value1, value2| (value1.to_f / value2.to_f).round(2)}
+#   end
+#
+#   def highest_win_percentage_by_team_id_helper
+#     team_id_with_ratio_of_shot = ratio_of_shots_to_goals_by_team_id_helper.find_all do |key, value|
+#       value == ratio_of_shots_to_goals_by_team_id_helper.values.max
+#     end
+#     team_id_variable = []
+#   team_id_with_ratio_of_shot.each do |id_and_ratio|
+#   team_id_variable << id_and_ratio[0]
+#     end
+#     team_id_variable
+#   end
+#
+#   def most_accurate_team
+#     team_name = @teams.find do |row|
+#       row[0] == highest_win_percentage_by_team_id_helper[0]
+#     end
+#     team_name[2]
+#   end
+#
+#   def lowest_win_percentage_by_team_id_helper
+#
+#     team_id_with_ratio_of_shot = ratio_of_shots_to_goals_by_team_id_helper.find_all do |key, value|
+#       value == ratio_of_shots_to_goals_by_team_id_helper.values.min
+#     end
+#     team_id_variable = []
+#   team_id_with_ratio_of_shot.each do |id_and_ratio|
+#   team_id_variable << id_and_ratio[0]
+#     end
+#     team_id_variable
+#   end
+#
+#   def least_accurate_team
+#     team_name = @teams.find do |row|
+#       row[0] == lowest_win_percentage_by_team_id_helper[0]
+#     end
+#     team_name[2]
+#   end
+#
+#   def total_number_tackles_by_team_id_helper
+#
+#       total_tackles_by_team_hash = {}
+#       @game_teams.each do |row|
+#         if total_tackles_by_team_hash.include?(row[1])
+#           total_tackles_by_team_hash[row[1]] += row[8].to_i
+#         else
+#           total_tackles_by_team_hash[row[1]] = row[8].to_i
+#         end
+#       end
+#    total_tackles_by_team_hash
+#   end
+#
+#   def team_id_with_most_tackles_helper
+#
+#     team_id_with_total_tackles = total_number_tackles_by_team_id_helper.find_all do |key, value|
+#       value == total_number_tackles_by_team_id_helper.values.max
+#     end
+#     team_id_variable = []
+#   team_id_with_total_tackles.each do |id_and_ratio|
+#   team_id_variable << id_and_ratio[0]
+#     end
+#     team_id_variable
+#   end
+#
+#   def most_tackles
+#     team_name = @teams.find do |row|
+#       row[0] == team_id_with_most_tackles_helper[0]
+#     end
+#     team_name[2]
+#   end
+#
+#   def team_id_with_fewest_tackles_helper
+#
+#     team_id_with_total_tackles = total_number_tackles_by_team_id_helper.find_all do |key, value|
+#       value == total_number_tackles_by_team_id_helper.values.min
+#     end
+#     team_id_variable = []
+#   team_id_with_total_tackles.each do |id_and_ratio|
+#   team_id_variable << id_and_ratio[0]
+#     end
+#     team_id_variable
+#   end
+#
+#   def fewest_tackles
+#     team_name = @teams.find do |row|
+#       row[0] == team_id_with_fewest_tackles_helper[0]
+#     end
+#     team_name[2]
+#     end
+#   end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -41,8 +41,6 @@ class StatTrackerTest < Minitest::Test
 
   def test_get_number_of_teams
 
-    assert_equal 6, @stat_tracker.count_of_teams
-
     assert_equal 32, @stat_tracker.count_of_teams
 
   end
@@ -53,123 +51,127 @@ class StatTrackerTest < Minitest::Test
 
   # ************* SeasonStatistics Tests *************
 
+  # def test_game_id_by_season_helper
+  #
+  #   assert_equal 1323, @stat_tracker.game_id_by_season_helper("20132014").length
+  #
+  # end
+  #
   def test_total_games_played_by_coach_helper
 
-    expected = {"John Tortorella"=>5, "Claude Julien"=>9, "Dan Bylsma"=>4, "Mike Babcock"=>1}
-
-
-    assert_equal expected, @stat_tracker.total_games_played_by_coach_helper
+    assert_equal 2646, @stat_tracker.total_games_played_by_coach_helper("20132014").length
   end
+  #
+  # def test_total_games_played_into_hash_helper
+  #
+  #   assert_equal 34, @stat_tracker.total_games_played_into_hash_helper("20132014").length
+  # end
+  #
+  # def test_total_games_won_by_coach_array_helper
+  #   assert_equal 1004, @stat_tracker.total_games_won_by_coach_array_helper("20132014").length
+  # end
+  #
+  # def test_games_won_into_hash_helper
+  #
+  #   assert_equal 33, @stat_tracker.games_won_into_hash_helper("20132014").length
+  # end
+  #
+  # def test_coaches_with_games_played_and_won_array_helper
+  #
+  #   assert_equal 33, @stat_tracker.coaches_with_games_played_and_won_array_helper("20132014").length
+  # end
 
-  def test_total_games_won_by_coach_array_helper
-    assert_equal ["Claude Julien", "Claude Julien", "Claude Julien", "Claude Julien", "Claude Julien", "Claude Julien", "Claude Julien", "Claude Julien", "Claude Julien",], @stat_tracker.total_games_won_by_coach_array_helper
-  end
+#   def test_coaches_winning_percentage_hash_helper
+#
+#     assert_equal [] , @stat_tracker.coaches_winning_percentage_hash_helper("20132014")
+#   end
+# end
+  def test_winningest_coach
 
-  def test_games_won_into_hash_helper
-
-    expected = {"Claude Julien"=>9}
-
-    assert_equal expected, @stat_tracker.games_won_into_hash_helper
-  end
-
-  def test_coaches_with_games_played_and_won_array_helper
-
-    assert_equal ["Claude Julien"], @stat_tracker.coaches_with_games_played_and_won_array_helper
-
-  end
-
-  def test_coaches_winning_percentage_hash_helper
-
-    expected = {"Claude Julien"=>100}
-
-    assert_equal expected , @stat_tracker.coaches_winning_percentage_hash_helper
-  end
-
-  def test_name_of_coach_with_highest_win_percentage
-
-    assert_equal "Claude Julien", @stat_tracker.winningest_coach
-  end
-
-  def test_worst_coach_if_someone_doesnt_have_any_wins_helper
-
-    assert_equal ["John Tortorella", "Dan Bylsma", "Mike Babcock"], @stat_tracker.worst_coach_if_someone_doesnt_have_any_wins_helper
-  end
-
-  def test_worst_coach_if_everyone_has_a_win_helper
-
-    assert_equal ["Claude Julien"], @stat_tracker.worst_coach_if_everyone_has_a_win_helper
-  end
-
-  def test_worst_coach
-
-    assert_equal "John Tortorella, Dan Bylsma, Mike Babcock", @stat_tracker.worst_coach
-  end
-
-  def test_total_goals_by_team_helper
-
-    expected = {"3"=>8, "6"=>24, "5"=>2, "17"=>1}
-
-    assert_equal expected, @stat_tracker.total_goals_by_team_id_hash_helper
-  end
-
-  def test_total_shots_by_team_id_hash_helper
-
-    expected = {"3"=>38, "6"=>76, "5"=>32, "17"=>5}
-
-    assert_equal expected, @stat_tracker.total_shots_by_team_id_hash_helper
-  end
-
-  def test_ratio_of_shots_to_goals_by_team_id_helper
-
-    expected = {"3"=>0.21, "6"=>0.32, "5"=>0.06, "17"=>0.2}
-
-    assert_equal expected, @stat_tracker.ratio_of_shots_to_goals_by_team_id_helper
-  end
-
-  def test_highest_win_percentage_by_team_id_helper
-
-    assert_equal ["6"], @stat_tracker.highest_win_percentage_by_team_id_helper
-  end
-
-  def test_name_of_team_with_best_shots_to_goal
-
-    assert_equal "FC Dallas", @stat_tracker.most_accurate_team
-  end
-
-  def test_lowest_win_percentage_by_team_id_helper
-
-    assert_equal ["5"], @stat_tracker.lowest_win_percentage_by_team_id_helper
-  end
-
-  def test_name_of_least_accurate_team
-
-    assert_equal "Sporting Kansas City", @stat_tracker.least_accurate_team
-  end
-
-  def test_total_number_tackles_by_team_helper
-
-    expected = {"3"=>179, "6"=>271, "5"=>150, "17"=>43}
-
-    assert_equal expected, @stat_tracker.total_number_tackles_by_team_id_helper
-  end
-
-  def test_team_id_with_most_tackles_helper
-
-    assert_equal ["6"], @stat_tracker.team_id_with_most_tackles_helper
-  end
-
-  def test_name_of_team_most_tackles
-
-    assert_equal "FC Dallas", @stat_tracker.most_tackles
-  end
-
-  def test_team_id_with_fewest_tackles_helper
-
-    assert_equal ["17"], @stat_tracker.team_id_with_fewest_tackles_helper
-  end
-
-  def test_name_of_team_fewest_tackles
-
-    assert_equal "LA Galaxy", @stat_tracker.fewest_tackles
+    assert_equal "Claude Julien", @stat_tracker.winningest_coach("20132014")
   end
 end
+
+#   def test_worst_coach_if_someone_doesnt_have_any_wins_helper
+#
+#     assert_equal ["John Tortorella", "Dan Bylsma", "Mike Babcock"], @stat_tracker.worst_coach_if_someone_doesnt_have_any_wins_helper
+#   end
+#
+#   def test_worst_coach_if_everyone_has_a_win_helper
+#
+#     assert_equal ["Claude Julien"], @stat_tracker.worst_coach_if_everyone_has_a_win_helper
+#   end
+#
+#   def test_worst_coach
+#
+#     assert_equal "John Tortorella, Dan Bylsma, Mike Babcock", @stat_tracker.worst_coach
+#   end
+#
+#   def test_total_goals_by_team_helper
+#
+#     expected = {"3"=>8, "6"=>24, "5"=>2, "17"=>1}
+#
+#     assert_equal expected, @stat_tracker.total_goals_by_team_id_hash_helper
+#   end
+#
+#   def test_total_shots_by_team_id_hash_helper
+#
+#     expected = {"3"=>38, "6"=>76, "5"=>32, "17"=>5}
+#
+#     assert_equal expected, @stat_tracker.total_shots_by_team_id_hash_helper
+#   end
+#
+#   def test_ratio_of_shots_to_goals_by_team_id_helper
+#
+#     expected = {"3"=>0.21, "6"=>0.32, "5"=>0.06, "17"=>0.2}
+#
+#     assert_equal expected, @stat_tracker.ratio_of_shots_to_goals_by_team_id_helper
+#   end
+#
+#   def test_highest_win_percentage_by_team_id_helper
+#
+#     assert_equal ["6"], @stat_tracker.highest_win_percentage_by_team_id_helper
+#   end
+#
+#   def test_name_of_team_with_best_shots_to_goal
+#
+#     assert_equal "FC Dallas", @stat_tracker.most_accurate_team
+#   end
+#
+#   def test_lowest_win_percentage_by_team_id_helper
+#
+#     assert_equal ["5"], @stat_tracker.lowest_win_percentage_by_team_id_helper
+#   end
+#
+#   def test_name_of_least_accurate_team
+#
+#     assert_equal "Sporting Kansas City", @stat_tracker.least_accurate_team
+#   end
+#
+#   def test_total_number_tackles_by_team_helper
+#
+#     expected = {"3"=>179, "6"=>271, "5"=>150, "17"=>43}
+#
+#     assert_equal expected, @stat_tracker.total_number_tackles_by_team_id_helper
+#   end
+#
+#   def test_team_id_with_most_tackles_helper
+#
+#     assert_equal ["6"], @stat_tracker.team_id_with_most_tackles_helper
+#   end
+#
+#   def test_name_of_team_most_tackles
+#
+#     assert_equal "FC Dallas", @stat_tracker.most_tackles
+#   end
+#
+#   def test_team_id_with_fewest_tackles_helper
+#
+#     assert_equal ["17"], @stat_tracker.team_id_with_fewest_tackles_helper
+#   end
+#
+#   def test_name_of_team_fewest_tackles
+#
+#     assert_equal "LA Galaxy", @stat_tracker.fewest_tackles
+#   end
+# end
