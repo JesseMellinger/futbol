b2193c819db4fa076b9c7c81137fef4645b45b09
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-12 13:06:18 -0600

    Create TeamManager test class

---
+++ test/team_manager_test.rb
@@ -0,0 +1,30 @@
+require './test/test_helper'
+require './lib/team_manager'
+require './lib/stat_tracker'
+
+class TeamManagerTest < Minitest::Test
+
+  def setup
+    @game_path = './data/games.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams.csv'
+
+
+    @locations = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(@locations)
+    @data = @stat_tracker.load_csv(@locations[:teams])
+    @team_manager = TeamManager.new(@data, @stat_tracker)
+
+  end
+
+  def test_it_exists
+    assert_instance_of TeamManager, @team_manager
+  end
+
+
+end
