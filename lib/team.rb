c138de5f3b7573da530e69d4698f76ba967514d8
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-12 13:56:38 -0600

    Create TeamTest class and test methods

--- lib/team.rb
+++ lib/team.rb
@@ -1,9 +1,11 @@
 class Team
+  attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :link,
+              :manager
 
   def initialize(row, manager)
     @team_id = row[:team_id]
-    @franchiseId = row[:franchiseId]
-    @teamName = row[:teamName]
+    @franchise_id = row[:franchiseid]
+    @team_name = row[:teamname]
     @abbreviation = row[:abbreviation]
     @link = row[:link]
     @manager = manager
