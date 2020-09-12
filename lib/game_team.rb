4626b9a63eb46e73ac17dfcfb19f9d7d344e532e
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-12 13:41:31 -0600

    Create GameTeam class and test methods

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -1,9 +1,11 @@
 class GameTeam
+  attr_reader :game_id, :team_id, :hoa, :result, :head_coach, :goals, :shots,
+              :tackles, :manager
 
   def initialize(row, manager)
     @game_id = row[:game_id]
     @team_id = row[:team_id]
-    @hoa = row[:HoA]
+    @hoa = row[:hoa]
     @result = row[:result]
     @head_coach = row[:head_coach]
     @goals = row[:goals]
