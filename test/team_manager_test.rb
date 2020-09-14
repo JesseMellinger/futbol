24770257655e5c625039ca3463ad9019f3a3aa9a
Eduardo Parra <helloeduardo@icloud.com>
2020-09-14 11:34:55 -0600

    Fix test instance variable

--- test/team_manager_test.rb
+++ test/team_manager_test.rb
@@ -72,11 +72,11 @@ class TeamManagerTest < Minitest::Test
   end
 
   def test_most_goals_scored
-    assert_equal 7, @stat_tracker.most_goals_scored("18")
+    assert_equal 7, @team_manager.most_goals_scored("18")
   end
 
   def test_fewest_goals_scored
-    assert_equal 0, @stat_tracker.fewest_goals_scored("18")
+    assert_equal 0, @team_manager.fewest_goals_scored("18")
   end
 
 end
