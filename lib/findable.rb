90dbf3b5215282eb9dd53fb29d0d8cbdd5c1b54c
Robert Heath <robert.b.heathii@gmail.com>
2020-09-15 16:52:48 -0500

    Add methods to findable

--- lib/findable.rb
+++ lib/findable.rb
@@ -1,2 +1,14 @@
 module Findable
+
+  def find_all_home_or_away_games(data, hoa)
+    @game_teams.find_all do |game_team|
+      game_team.hoa == hoa
+    end
+  end
+
+  def find_games_by_team(data, team_id)
+    @game_teams.find_all do |game_team|
+      game_team.team_id == team_id
+    end
+  end
 end
